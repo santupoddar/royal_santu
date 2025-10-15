0104932f4e203a856a76c5ccb5190c14b68f5e13
Santu Poddar <santuwebskitters@gmail.com>
2025-10-15 17:55:45 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let icon = document.querySelector(".icon");
+let ul = document.querySelector("ul");
+icon.addEventListener("click",()=>{
+    ul.classList.toggle("showData");
+    console.log(ul);
+});
